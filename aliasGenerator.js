8c0aa3b4c85e07abc729e29987797b21a325b7b2
KSFlynn007 <kelseyflynnn@gmail.com>
2021-10-28 14:27:17 -0600

    added single load

--- aliasGenerator.js
+++ aliasGenerator.js
@@ -6,6 +6,7 @@ const outputMore = document.querySelector('.outputMore');
 const inputVal = document.querySelector('.listNumber');
 
 generateOne.addEventListener('click', (e) => {
+    outputOne.innerHTML = '';
     loadOne(url)
 })
 
@@ -13,17 +14,46 @@ function loadOne(url){
     fetch(url).then((rep) => rep.json())
     .then((data) => {
         let response = data.results[0];
-        console.log(response);
+        // console.log(response);
+        makeAlias(response);
+        outputOne.scrollIntoView({
+            behavior: "smooth",
+        });
     })
     .catch((err) => {
         console.log(err)
     }) 
 }
 
+function makeAlias(element){
+    console.log(element);
+    const div1 = eleMaker('div', outputOne, '');
+    div1.classList.add('singleBox');
+    div1.classList.add('resultsFont')
 
+    const name = `<span>${element.name.title} ${element.name.first} ${element.name.last}</span>`;
+    const email = `<span>Email</span> : ${element.email}`
+    const dob = `${element.dob.date}`
+    const age = `<span>DOB</span> : ` + dob.slice(0,10);
+    const img = `<img src="${element.picture.large}">`;
+    const address = `<span>Address</span> : ${element.location.street.number} ${element.location.street.name}, ${element.location.city}, ${element.location.state}, ${element.location.country}`;
+    const postcode = `<span>Postal Code</span> : ${element.location.postcode}`;
+    const phone = `<span>Phone Number</span> : ${element.phone}`
+
+    eleMaker('div', div1, name);
+    eleMaker('div', div1, age);
+    eleMaker('div', div1, img);
+    eleMaker('div', div1, address);
+    eleMaker('div', div1, postcode);
+    eleMaker('div', div1, email);
+    eleMaker('div', div1, phone);
+}
+
+// if statement for grouping countries into continents needed!
 
 
 generateMore.addEventListener('click', (e) => {
+    outputMore.innerHTML = '';
     let listVal = `?results=${inputVal.value}`;
     loadMore(url + listVal);
 })
@@ -34,6 +64,9 @@ function loadMore(url){
         let response = data.results;
         // console.log(response);
         makeList(response);
+        outputMore.scrollIntoView({
+            behavior: "smooth",
+        });
     })
     .catch((err) => {
         console.log(err)
@@ -42,29 +75,27 @@ function loadMore(url){
 
 function makeList(data){
     data.forEach(element => {
-        console.log(element);
-        const div = eleMaker('div', outputMore, '');
-        div.classList.add('box');
+        // console.log(element);
+        const div2 = eleMaker('div', outputMore, '');
+        div2.classList.add('box');
+        div2.classList.add('resultsFont')
 
-
-        const name = `${element.name.title} ${element.name.first} ${element.name.last}`;
-        const email = `Email : ${element.email}`
+        const name = `<span>${element.name.title} ${element.name.first} ${element.name.last}</span>`;
+        const email = `<span>Email</span> : ${element.email}`
         const dob = `${element.dob.date}`
-        const age = `DOB : ` + dob.slice(0,10);
+        const age = `<span>DOB</span> : ` + dob.slice(0,10);
         const img = `<img src="${element.picture.large}">`;
-        const address = `Address : ${element.location.street.number} ${element.location.street.name}, ${element.location.city}, ${element.location.state}, ${element.location.country}`;
-        const postcode = `Postal Code : ${element.location.postcode}`;
-        const phone = `Phone Number : ${element.phone}`
+        const address = `<span>Address</span> : ${element.location.street.number} ${element.location.street.name}, ${element.location.city}, ${element.location.state}, ${element.location.country}`;
+        const postcode = `<span>Postal Code</span> : ${element.location.postcode}`;
+        const phone = `<span>Phone Number</span> : ${element.phone}`
 
-        eleMaker('div', div, name);
-        eleMaker('div', div, age);
-        eleMaker('div', div, img);
-        eleMaker('div', div, address);
-        eleMaker('div', div, postcode);
-        eleMaker('div', div, email);
-        eleMaker('div', div, phone);
-
-
+        eleMaker('div', div2, name);
+        eleMaker('div', div2, age);
+        eleMaker('div', div2, img);
+        eleMaker('div', div2, address);
+        eleMaker('div', div2, postcode);
+        eleMaker('div', div2, email);
+        eleMaker('div', div2, phone);
     });
 }
 
@@ -74,4 +105,3 @@ function eleMaker(eleTag, parent, contents){
     boxResult.innerHTML = contents;
     return boxResult;
 }
-// if statement for grouping countries into continents needed!
